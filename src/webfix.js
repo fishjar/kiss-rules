804c1dbfde14cbddeff8f5e6491aa3cfe8227a90
Gabe Yuan <yugang2002@gmail.com>
2023-09-09 23:23:23 +0800

    add baidu.com fixer

--- src/webfix.js
+++ src/webfix.js
@@ -1,4 +1,5 @@
 const FIXER_BR = "br";
+const WEBFIX_FONTSIZE = "fontSize";
 
 export default [
   {
@@ -8,9 +9,15 @@ export default [
     fixer: FIXER_BR,
   },
   {
-    pattern: "t.me/s/*",
+    pattern: "t.me/s/",
     selector: ".tgme_widget_message_text",
     rootSlector: ".tgme_channel_history",
     fixer: FIXER_BR,
+  },
+  {
+    pattern: "baidu.com",
+    selector: "html",
+    rootSlector: "",
+    fixer: WEBFIX_FONTSIZE,
   },
 ];
