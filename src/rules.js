707d3ab4ccfcc2515f8fa76a3009db37bcf06233
Gabe Yuan <yugang2002@gmail.com>
2024-04-06 22:04:21 +0800

    fix rule: youtube

--- src/rules.js
+++ src/rules.js
@@ -151,7 +151,7 @@ const RULES_MAP = {
     selector: `h1>.ytd-watch-metadata, #video-title, #title, #content-text>span, #snippet-text, yt-attributed-string.ytd-text-inline-expander>span, .ytp-caption-segment`, // 翻译字幕
     // selector: `h1>.ytd-watch-metadata, #video-title, #title, #content-text>span, #snippet-text, yt-attributed-string.ytd-text-inline-expander>span`, // 不翻译字幕
     keepSelector: `img, #content-text>a, span[dir="auto"], span:has(img)`,
-    injectCss: `ytd-expander[should-use-number-of-lines][collapsed]>#content.ytd-expander { -webkit-line-clamp: unset; max-height: none; height: auto; }`,
+    // injectCss: `ytd-expander[should-use-number-of-lines][collapsed]>#content.ytd-expander { -webkit-line-clamp: unset; max-height: none; height: auto; }`,
   },
   "bard.google.com": {
     selector: `.query-content ${DEFAULT_SELECTOR}, message-content ${DEFAULT_SELECTOR}`,
