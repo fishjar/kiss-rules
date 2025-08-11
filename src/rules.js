9be94d568eacb2f5b04443b50e3223f2f6c2af19
Gabe <yugang2002@gmail.com>
2025-08-11 15:53:27 +0800

    Update rules.js -> gogole.com

--- src/rules.js
+++ src/rules.js
@@ -35,7 +35,7 @@ const DEFAULT_RULE = {
 
 const RULES_MAP = {
   "www.google.com/search": {
-    selector: `h3, .IsZvec, .VwiC3b, .nDgy9d, .ITZIwc`,
+    selector: `h3, .IsZvec, .VwiC3b, .nDgy9d, .ITZIwc, .HzN33d>span`,
   },
   "news.google.com": {
     selector: `[data-n-tid], ${DEFAULT_SELECTOR}`,
